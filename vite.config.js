19b4d53ddd4d8670514d7c7b61a80044fed99ce4
Pier Paolo Poggi Pollini <pier-mac@Air-di-Pier.homenet.telecomitalia.it>
2022-12-24 02:18:14 +0100

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-// vite.config.js
-export default {
-    'base': '/ <tenzies-game> /'
-    // config options
-}
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+
+// https://vitejs.dev/config
+export default defineConfig ({
+    base: '/tenzies-game/',
+    plugins: [react()]
+})
